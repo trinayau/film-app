a9b1a8564fd8183c47b26f357cec7a0b68b534f0
Peritract <dan@getfutureproof.co.uk>
2022-05-06 09:43:19 +0100

    Add back button back

--- src/pages/WorkPage/index.js
+++ src/pages/WorkPage/index.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
+import { useNavigate, useParams } from 'react-router';
 import axios from 'axios';
 
 const WorkPage = () => {
 
     const { name } = useParams();
     const [showData, setShowData] = useState({});
+    const navigate = useNavigate();
 
     useEffect(() => {
 
@@ -25,6 +26,7 @@ const WorkPage = () => {
     return <>
             <h1>{showData.name}</h1>
             <div dangerouslySetInnerHTML={{__html: showData.summary}}></div>
+            <button onClick={() => {navigate(-1)}}>Back</button>
            </>
 }
 
